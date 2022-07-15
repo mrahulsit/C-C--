5e0a4b344b3245b13bc618a6da33310245d7ad04
mrahulsit <mrahuls@gmail.com>
2022-07-15 13:16:56 +0530

    git commit

--- Stack/q4.c
+++ Stack/q4.c
@@ -1,57 +1,98 @@
-#include <stdio.h>
-#include <string.h>
- 
-int stack[100],top=-1,size;
-int isFull() {   
-    if(top >= size-1)
-        return 1;
-    else
-        return 0;
+/*  C Program To Check for Balanced Parentheses using Stack*/
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define MAX 30
+int top=-1;
+int stack[MAX];
+
+void push(char);
+char pop();
+int match(char a,char b);
+int check(char []);
+
+int main()
+{
+        char exp[MAX];
+        int valid;
+        printf("Enter an algebraic expression : ");
+        gets(exp);
+        valid=check(exp);
+        if(valid==1)
+                printf("Valid expression\n");
+        else
+                printf("Invalid expression\n");
+
+                return 0;
+
 }
-int isEmpty() {
- if(top == -1)
-     return 1;
- else
-     return 0;
-}
-void push(int num) {
-    if (isFull())
-        printf("Stack is Full...\n");
-    else {
-        stack[top] = num;
-        top++;
-    }
-}
-int pop() {
-    if (isEmpty())
-        printf("Stack is Empty...\n");
-    else {
-     top = top - 1;
-    }
-}
- 
-int main() {
-    char inputString[100], c;
-    int i, length;
-    initialize();
-    printf("Enter a string of paranthesis\n");
-    gets(inputString);
-    length = strlen(inputString);
-    for(i = 0; i < length; i++){
-        if(inputString[i] == '{')
-            push(inputString[i]);
-        else if(inputString[i] == '}')
-            pop();
-        else {
-            printf("Error : Invalid Character !! \n");
-            return 0;
+int check(char exp[] )
+{
+        int i;
+        char temp;
+        for(i=0;i<strlen(exp);i++)
+        {
+                if(exp[i]=='(' || exp[i]=='{' || exp[i]=='[')
+                        push(exp[i]);
+                if(exp[i]==')' || exp[i]=='}' || exp[i]==']')
+                        if(top==-1)    /*stack empty*/
+                        {
+                                printf("Right parentheses are more than left parentheses\n");
+                                return 0;
+                        }
+                        else
+                        {
+                                temp=pop();
+                                if(!match(temp, exp[i]))
+                                {
+                                        printf("Mismatched parentheses are : ");
+                                        printf("%c and %c\n",temp,exp[i]);
+                                        return 0;
+                                }
+                        }
         }
-    }
-     
-    if(isEmpty())
-        printf("Valid Paranthesis Expression\n");
-    else
-        printf("InValid Paranthesis Expression\n");
-         
-    return 0;
-}
+        if(top==-1) /*stack empty*/
+        {
+                printf("Balanced Parentheses\n");
+                return 1;
+        }
+        else
+        {
+                printf("Left parentheses more than right parentheses\n");
+                return 0;
+        }
+}/*End of main()*/
+int match(char a,char b)
+{
+        if(a=='[' && b==']')
+                return 1;
+        if(a=='{' && b=='}')
+                return 1;
+        if(a=='(' && b==')')
+                return 1;
+        return 0;
+}/*End of match()*/
+
+void push(char item)
+{
+        if(top==(MAX-1))
+        {
+                printf("Stack Overflow\n");
+                return;
+        }
+        top=top+1;
+        stack[top]=item;
+}/*End of push()*/
+
+char pop()
+{
+        if(top==-1)
+        {
+                printf("Stack Underflow\n");
+                exit(1);
+        }
+        return(stack[top--]);
+}/*End of pop()*/
